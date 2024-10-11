9a1476389e9d88f62c9860375ccd3566ddc1a322
Connor Frendt <connorfrendt@gmail.com>
2024-10-11 10:09:54 -0700

    data able to be passed down to phonetype comp

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
     <div id="app">
         <div style="display: flex; justify-content: center;">
-                <select id="dropdown">
-                    <option id="option-0" value=""></option>
-                    <option id="option-1" value="phone.json">Phone</option>
-                    <option id="option-2" value="10130.json">10130</option>
-                    <option id="option-3" value="12455.json">12455</option>
-                </select>
-            </div>
-        <PhoneType />
+            <select id="dropdown" v-model="selectedValue">
+                <option id="option-0" value=""></option>
+                <option id="option-1" value="phone.json">Phone</option>
+                <option id="option-2" value="10130.json">10130</option>
+                <option id="option-3" value="12455.json">12455</option>
+            </select>
+        </div>
+        <div></div>
+        <PhoneType :phoneType="phoneType" />
     </div>
 </template>
 
@@ -16,6 +17,18 @@
 import PhoneType from './components/PhoneType.vue';
 export default {
     name: 'App',
+    data() {
+        return {
+            userInput: [],
+            phoneType: 'hellooooooo',
+            selectedValue: ''
+        }
+    },
+    watch: {
+        selectedValue(newValue) {
+            this.phoneType = newValue;
+        }
+    },
     components: {
         PhoneType
     }
--- src/components/PhoneType.vue
+++ src/components/PhoneType.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>Hello World</div>
+    <div>
+        <div>Phone Type Component</div>
+        <div>{{ phoneType }}</div>
+    </div>
 </template>
 
 <script>
-
+export default {
+    props: {
+        phoneType: {
+            type: String,
+            required: true
+        }
+    }
+}
 </script>
 
 <style>
