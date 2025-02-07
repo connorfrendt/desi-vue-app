2a5dc0272ef9a8821b572d125cbb78b962ea9472
Connor Frendt <connorfrendt@gmail.com>
2025-02-06 19:28:51 -0800

    wip

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const path = require('path');
 
 console.log('******************************');
 app.get('/api/files', (req, res) => {
-    // console.log('APP GET');
-    // console.log('sub dir?', req.query);
     const subdirectory = req.query.subdirectory || '';
     const directoryPath = path.join(__dirname, 'public/json/JSON-files/', subdirectory);
     
@@ -27,7 +25,6 @@ app.get('/api/files/:subdirectory/:filename', (req, res) => {
     const filePath = path.join(__dirname, 'public/json/JSON-files/', subdirectory, filename);
     
     fs.readFile(filePath, 'utf8', (err, data) => {
-        console.log('***********DATA*********\n', data);
         if (err) {
             return res.status(500).send('Unable to read file: ' + err);
         }
