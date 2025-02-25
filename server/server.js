c2a2c6c93abc23ea34be5ba1723583deb84c141f
Connor Frendt <connorfrendt@gmail.com>
2025-02-25 09:08:28 -0800

    modified file arch, separated front and back end

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const fs = require('fs');
+const path = require('path');
+
+console.log('******************************');
+app.get('/api/files', (req, res) => {
+    const subdirectory = req.query.subdirectory || '';
+    const directoryPath = path.join(__dirname, '../app/public/json/new-json-files/', subdirectory);
+    fs.readdir(directoryPath, (err, files) => {
+        if(err) {
+            return res.status(500).send('Unable to scan directory: ' + err);
+        }
+        res.json(files);
+        return files;
+    });
+});
+
+app.get('/api/files/:subdirectory/:filename', (req, res) => {
+    const subdirectory = req.params.subdirectory;
+    const filename = req.params.filename;
+    const filePath = path.join(__dirname, '../app/public/json/new-json-files/', subdirectory, filename);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Unable to read file: ', err);
+            return res.status(500).send('Unable to read file: ' + err);
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Started on port: ${PORT}`);
+});
