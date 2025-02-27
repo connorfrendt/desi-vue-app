e09e00eb6780c7b23fe3416ddb13013ddc9f4adc
Connor Frendt <connorfrendt@gmail.com>
2025-02-27 10:36:17 -0800

    wip

--- server/lib/app.js
+++
@@ -1,9 +0,0 @@
-import PocketBase from 'pocketbase';
-
-const pb = new PocketBase('http://localhost:8090');
-
-const authData = await pb.collection("users").authWithPassword('test@example.com', '123456');
-
-console.log(pb.authStore.isValid);
-console.log(pb.authStore.token);
-console.log(pb.authStore.record.id);
---
+++ server/lib/db-client.js
@@ -0,0 +1,16 @@
+import PocketBase from 'pocketbase';
+console.log('******************************');
+const DATABASE_URL = 'http://localhost:8090';
+console.log('******************************');
+
+const pb = new PocketBase(DATABASE_URL);
+console.log('******************************');
+
+const authData = await pb.collection("users").authWithPassword('test@example.com', '12345678');
+
+console.log('******************************');
+// console.log(pb.authStore.isValid);
+// console.log(pb.authStore.token);
+// console.log(pb.authStore.record.id);
+
+module.exports = pb;
