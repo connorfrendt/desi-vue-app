d99a95a9eb3763f4abff5995df544cf9463471c0
Connor Frendt <connorfrendt@gmail.com>
2025-05-01 16:53:05 -0700

    idk man just trying to get this to work

--- server/main.go
+++ server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+    "log"
+
     "github.com/pocketbase/pocketbase"
 )
 
 func main() {
     app := pocketbase.New()
-    app.Start()
+
+    if err := app.Start(); err != nil {
+        log.Fatal(err)
+    }
 }
