73b3b7db1d0222bf2cff6e78fc0c77276c51f2bc
Connor Frendt <connorfrendt@gmail.com>
2025-05-05 14:17:51 -0700

    trying to start pocketbase fresh to make account

--- server/main.go
+++ server/main.go
@@ -7,8 +7,12 @@ import (
 )
 
 func main() {
-    app := pocketbase.New()
+    // Create PocketBase app with custom data directory
+    app := pocketbase.NewWithConfig(pocketbase.Config{
+        DataDir: "./pb_data", // This ensures data is stored in a known, clean location
+    })
 
+    // Start the app and log fatal error if startup fails
     if err := app.Start(); err != nil {
         log.Fatal(err)
     }
