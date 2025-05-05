24ad5ecec49ed1601dce7f6bc33c11b7494b374a
Connor Frendt <connorfrendt@gmail.com>
2025-05-05 14:23:06 -0700

    reverting main go

--- server/main.go
+++ server/main.go
@@ -7,12 +7,8 @@ import (
 )
 
 func main() {
-    // Create PocketBase app with custom data directory
-    app := pocketbase.NewWithConfig(pocketbase.Config{
-        DataDir: "./pb_data", // This ensures data is stored in a known, clean location
-    })
+    app := pocketbase.New()
 
-    // Start the app and log fatal error if startup fails
     if err := app.Start(); err != nil {
         log.Fatal(err)
     }
