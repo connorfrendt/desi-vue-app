ad047125d7235ca216a2734d4af3c1aa1d1a4356
Connor Frendt <connorfrendt@gmail.com>
2025-05-06 14:30:06 -0700

    trying...again...

--- server/main.go
+++ server/main.go
@@ -1,37 +1,39 @@
 package main
 
 import (
-	"log"
-	"os"
+    "log"
+    "os"
 
-	"github.com/pocketbase/pocketbase"
+    "github.com/pocketbase/pocketbase"
+    "github.com/pocketbase/pocketbase/core"
+    "github.com/pocketbase/pocketbase/models"
 )
 
 func main() {
-	app := pocketbase.New()
+    app := pocketbase.New()
 
-	app.App().OnBeforeServe().Add(func(e any) error {
-		admins, err := app.Dao().FindAdmins()
-		if err != nil {
-			return err
-		}
+    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+        admins, err := app.Dao().FindAdmins()
+        if err != nil {
+            return err
+        }
 
-		if len(admins) == 0 {
-			log.Println("No admin users found. Creating default admin...")
+        if len(admins) == 0 {
+            log.Println("No admin users found. Creating default admin...")
 
-			admin := app.NewAdmin()
-			admin.Email = os.Getenv("PB_ADMIN_EMAIL")
-			admin.SetPassword(os.Getenv("PB_ADMIN_PASS"))
+            admin := &models.Admin{}
+            admin.Email = os.Getenv("PB_ADMIN_EMAIL")
+            admin.SetPassword(os.Getenv("PB_ADMIN_PASS"))
 
-			if err := app.Dao().SaveAdmin(admin); err != nil {
-				return err
-			}
-		}
+            if err := app.Dao().SaveAdmin(admin); err != nil {
+                return err
+            }
+        }
 
-		return nil
-	})
+        return nil
+    })
 
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
+    if err := app.Start(); err != nil {
+        log.Fatal(err)
+    }
 }
