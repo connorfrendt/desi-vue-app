0af175e097c567fcda38cdcaf851d8d191386d99
Connor Frendt <connorfrendt@gmail.com>
2025-05-06 15:36:02 -0700

    trying something

--- server/main.go
+++ server/main.go
@@ -11,26 +11,26 @@ import (
 func main() {
     app := pocketbase.New()
 
-    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-        admins, err := app.Dao().FindAdmins()
-        if err != nil {
-            return err
-        }
+    // app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+    //     admins, err := app.Dao().FindAdmins()
+    //     if err != nil {
+    //         return err
+    //     }
 
-        if len(admins) == 0 {
-            log.Println("No admin users found. Creating default admin...")
+    //     if len(admins) == 0 {
+    //         log.Println("No admin users found. Creating default admin...")
 
-            admin := app.NewAdmin()
-            admin.Email = os.Getenv("PB_ADMIN_EMAIL")
-            admin.SetPassword(os.Getenv("PB_ADMIN_PASS"))
+    //         admin := app.NewAdmin()
+    //         admin.Email = os.Getenv("PB_ADMIN_EMAIL")
+    //         admin.SetPassword(os.Getenv("PB_ADMIN_PASS"))
 
-            if err := app.Dao().SaveAdmin(admin); err != nil {
-                return err
-            }
-        }
+    //         if err := app.Dao().SaveAdmin(admin); err != nil {
+    //             return err
+    //         }
+    //     }
 
-        return nil
-    })
+    //     return nil
+    // })
 
     if err := app.Start(); err != nil {
         log.Fatal(err)
