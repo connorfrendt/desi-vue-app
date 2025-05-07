6aba6a559c6816b10896111c0ee305b89565168c
Connor Frendt <connorfrendt@gmail.com>
2025-05-07 13:24:16 -0700

    trying this blank

--- server/main.go
+++ server/main.go
@@ -2,42 +2,11 @@ package main
 
 import (
     "log"
-	"os"
     "github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-
-	port := os.Getenv("PORT")
-	
 	app := pocketbase.New()
-
-	if port != "" {
-		app.SetAddress(":" + port)
-	}
-    
-
-    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-        admins, err := app.Dao().FindAdmins()
-        if err != nil {
-            return err
-        }
-
-        if len(admins) == 0 {
-            log.Println("No admin users found. Creating default admin...")
-
-            admin := app.NewAdmin()
-            admin.Email = os.Getenv("PB_ADMIN_EMAIL")
-            admin.SetPassword(os.Getenv("PB_ADMIN_PASS"))
-
-            if err := app.Dao().SaveAdmin(admin); err != nil {
-                return err
-            }
-        }
-
-        return nil
-    })
 
     if err := app.Start(); err != nil {
         log.Fatal(err)
